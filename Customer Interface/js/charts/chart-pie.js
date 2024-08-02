41c33bc8dc4fd7aafd22a171a52e8881ee927177
wuguorongwuguorong <daniel.stillpixels@gmail.com>
2024-08-02 13:59:04 +0800

    Changes in the sales analysis outlook
    I have added a new sales analysis html. This html will include, charts for Best selling bread, top selling protein, revenue contributed from different age group, y-o-y sales, m-o-m sales and hourly sales data.

---
+++ Customer Interface/js/charts/chart-pie.js
@@ -0,0 +1,47 @@
+// Set new default font family and font color to mimic Bootstrap's default styling
+Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = '#858796';
+
+// Pie Chart Example
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ["Sourdough", "Wholemeal", "Oat Bread", "Italian Herb"],
+    datasets: [{
+      data: [55, 30, 15,90],
+      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc','#f6c23e'],
+      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf','#f6b23e'],
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    tooltips: {
+        backgroundColor: "rgb(255,255,255)",
+        bodyFontColor: "#858796",
+        borderColor: '#dddfeb',
+        borderWidth: 1,
+        xPadding: 15,
+        yPadding: 15,
+        displayColors: false,
+        caretPadding: 10,
+    },
+    legend: {
+        display: true, // Display the legend
+        position: 'bottom', // Position of the legend (top, bottom, left, right)
+        labels: {
+            fontColor: '#333', // Color of the labels
+            fontSize: 10, // Font size of the labels
+            usePointStyle: true, // Use point style (like circles instead of squares)
+        },
+    },
+    cutoutPercentage: 0,
+    layout: {
+        padding: {
+            top: 5,
+            bottom: 5, // Add space between the chart and the legend
+        }
+    },
+},
+});
